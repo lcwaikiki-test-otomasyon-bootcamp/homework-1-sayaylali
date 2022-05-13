298e1d8b71f8435f4ec7c263fcb6537bd4401f17
Serdar Alp Yaylalı <serdaralpyaylali@gmail.com>
2022-05-13 20:10:38 +0300

    Uyarı notu ekrana yazdırıldı

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,45 +3,47 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.io.Console;
 
 public class Main {
     //Ana Kodumuz
     public static void main(String[] args) {
-        //Chrome driveri kurar
+        //setting options to avoid automation detection
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-blink-features=AutomationControlled"); //removes the detection
+        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"}); //removes the banner
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(options);   //create the driver with the options
         //Siteye girer
         driver.get("https://www.lcwaikiki.com/tr-TR/TR/giris");
         try {
-            Thread.sleep(5000);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         //Full ekran
         driver.manage().window().maximize();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        // email ve şifre girer
-        driver.findElement(By.name("email")).sendKeys("yanlis@mail.com");
-        driver.findElement(By.name("password")).sendKeys("12.Sdse611");
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        // girişe tıklar
-        WebElement elem =driver.findElement(By.cssSelector("#login > div > div > div > div > form > button"));
-        //Bu kısımda uzun bir döngüye giriyor
+        // üyeol tıklar
+        WebElement elem =driver.findElement(By.cssSelector(".login-form__link[data-tracking-category]"));
         elem.click();
         try {
-            Thread.sleep(2000);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        //mail ve şifre girer
+        driver.findElement(By.cssSelector(".ym-disable-keys.TLeaf-Mask[data-val-placeholder=\"Lütfen e-posta adresinizi giriniz.\"]")).sendKeys("yanlişmail.com");
+        driver.findElement(By.id("RegisterFormView_Password")).sendKeys("12.Sdse611");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //Uyarı mesajını console'a yazdırır.
+        WebElement elem2 =driver.findElement(By.id("RegisterFormView_RegisterEmail-error"));
+        System.out.println(elem2.getText());
+
         }
 }
 
